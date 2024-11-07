2773e39c481901bc50418f7f987dab3901d0a8b5
pgrod <149545353+pgrod@users.noreply.github.com>
2024-11-06 23:41:10 -0600

    Hacer los 2 parametros e imprimir

--- EjercicioExcepciones/Operaciones.cs
+++ EjercicioExcepciones/Operaciones.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EjercicioExcepciones
+﻿namespace EjercicioExcepciones
 {
-    internal class Operaciones
+    public class Operaciones
     {
+        public double Sumar(double Digito1, double Digito2)
+        {
+            return Digito1 + Digito2;
+        }
+        public double Restar(double Digito1, double Digito2)
+        {
+            return Digito1 - Digito2;
+        }
+        public double Multiplicar(double Digito1, double Digito2)
+        {
+            return Digito1 * Digito2;
+        }
+        public double Dividir(double Digito1, double Digito2)
+        {
+            if(Digito2 == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero");
+            }
+            return Digito1 / Digito2;
+        }
     }
 }
--- EjercicioExcepciones/Program.cs
+++ EjercicioExcepciones/Program.cs
@@ -1,1 +1,58 @@
-﻿
+﻿using EjercicioExcepciones;
+Operaciones operaciones = new Operaciones();
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("Seleccione una opción: ");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("5. Salir");
+
+    try
+    {
+        int opcion = int.Parse(Console.ReadLine());
+        if (opcion == 5)
+        {
+            continuar = false;
+            continue;
+        }
+
+        Console.WriteLine("Ingrese el primer dígito: ");
+        double Digito1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo dígito: ");
+        double Digito2 = double.Parse(Console.ReadLine());
+        double resultado = 0;
+
+        switch (opcion)
+        {
+            case 1:
+                resultado = operaciones.Sumar(Digito1, Digito2);
+                break;
+
+            case 2:
+                resultado = operaciones.Restar(Digito1, Digito2);
+                break;
+
+            case 3:
+                resultado = operaciones.Multiplicar(Digito1, Digito2);
+                break;
+
+            case 4:
+                resultado = operaciones.Dividir(Digito1, Digito2);
+                break;
+
+            default:
+                Console.WriteLine("Opción no válida");
+                continue;
+        }
+        Console.WriteLine($"El resultado es: {resultado}");
+        Console.WriteLine();
+    }
+    catch(DivideByZeroException ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
